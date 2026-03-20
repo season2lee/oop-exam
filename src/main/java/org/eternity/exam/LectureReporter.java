c39a6d8d297a34d051410e25f5243682adb33e68
Lee Sijun <season2lee@gmail.com>
2026-03-20 18:21:38 +0900

    [feat] : 타입 추가되어도 설계에 문제 없도록 LectureReporter 작성

--- src/main/java/org/eternity/exam/LectureReporter.java
+++ src/main/java/org/eternity/exam/LectureReporter.java
@@ -1,59 +1,19 @@
 package org.eternity.exam;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.csv.CsvMapper;
-import com.fasterxml.jackson.dataformat.csv.CsvSchema;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.jdbc.core.simple.JdbcClient;
-
-import java.io.FileWriter;
-
 public class LectureReporter {
-    public enum FormatType { JSON, CSV, XML }
-    public enum StorageType { DATABASE, FILE }
 
-    private final JdbcClient jdbcClient;
+    private final FormatChecker formatChecker;
+    private final StorageChecker storageChecker;
 
-    public LectureReporter(JdbcClient jdbcClient) {
-        this.jdbcClient = jdbcClient;
+    public LectureReporter(FormatChecker formatChecker, StorageChecker storageChecker) {
+        this.formatChecker = formatChecker;
+        this.storageChecker = storageChecker;
     }
 
-    public void report(FormatType formatType, StorageType storageType, Lecture lecture) throws Exception {
-        String serialized = null;
-
+    public void report(Lecture lecture) throws Exception {
         // 포맷 변환
-        switch (formatType) {
-            case JSON -> {
-                ObjectMapper mapper = new ObjectMapper();
-                mapper.registerModule(new JavaTimeModule());
-                serialized = mapper.writeValueAsString(lecture);
-            }
-            case CSV -> {
-                CsvMapper mapper = new CsvMapper();
-                mapper.registerModule(new JavaTimeModule());
-                CsvSchema schema = mapper.schemaFor(Lecture.class).withHeader();
-                serialized = mapper.writer(schema).writeValueAsString(lecture);
-            }
-            case XML -> {
-                XmlMapper mapper = new XmlMapper();
-                mapper.registerModule(new JavaTimeModule());
-                serialized = mapper.writeValueAsString(lecture);
-            }
-        }
-
+        String serialized = formatChecker.formatCheck(lecture);
         //  저장
-        switch (storageType) {
-            case DATABASE -> {
-                jdbcClient.sql("INSERT INTO LECTURE(SERIALIZED_DATA) VALUES(?)")
-                        .param(1, serialized)
-                        .update();
-            }
-            case FILE -> {
-                try (FileWriter writer = new FileWriter("lecture_data.txt")) {
-                    writer.write(serialized);
-                }
-            }
-        }
+        storageChecker.save(serialized);
     }
 }
